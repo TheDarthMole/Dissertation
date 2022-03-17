8e52b01a2018d7a01427d3b2c73edfb0f0730bf8
TheDarthMole <27959011+TheDarthMole@users.noreply.github.com>
2022-03-17 12:58:49 +0000

    chore: refactored imports

--- apps/progression/views.py
+++ apps/progression/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.template import loader
-from django.urls import reverse
-from django.shortcuts import redirect, render
-from apps.learn.models import Lesson, ExploitType
+from apps.learn.models import ExploitType
 from apps.progression.models import Progression
-from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
