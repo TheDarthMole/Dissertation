6bb66c0a8dc95838ab76831b6e59a9e6ef821159
TheDarthMole <27959011+TheDarthMole@users.noreply.github.com>
2022-04-25 16:41:24 +0100

    cont: CodeMirror is being buggy AGAIN... Installing either the @codemirror/commands or @codemirror/view packages stops CodeMirror working, not even implementing it...

--- apps/static/assets/js/editor.bundle.js
+++ apps/static/assets/js/editor.bundle.js
@@ -22715,44 +22715,49 @@
         return new LanguageSupport(javaLanguage);
     }
 
-    // <script src="webjars/codemirror/5.22.0/lib/codemirror.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/mode/clike/clike.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/mode/diff/diff.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/dialog/dialog.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/search/searchcursor.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/search/search.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/scroll/annotatescrollbar.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/search/matchesonscrollbar.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/search/jump-to-line.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/selection/active-line.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/edit/matchbrackets.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/edit/closebrackets.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/hint/show-hint.js" type="text/javascript" ></script>
-        // <script src="webjars/codemirror/5.22.0/addon/hint/anyword-hint.js" type="text/javascript" ></script>
+    // import {keymap} from "@codemirror/view"
+    // import {indentWithTab} from "@codemirror/commands"
+
+    // <script src="webjars/codemirror/5.22.0/addon/dialog/dialog.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/search/searchcursor.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/search/search.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/scroll/annotatescrollbar.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/search/matchesonscrollbar.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/search/jump-to-line.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/selection/active-line.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/edit/matchbrackets.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/edit/closebrackets.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/hint/show-hint.js" type="text/javascript" ></script>
+    // <script src="webjars/codemirror/5.22.0/addon/hint/anyword-hint.js" type="text/javascript" ></script>
+
 
 
     var editor_element = document.getElementById('editor');
 
     // Convert from base64
     var original_code = atob(editor_element.textContent);
-    editor_element.textContent='';
+    editor_element.textContent = '';
 
 
     let editor = new EditorView({
-      state: EditorState.create({
-        extensions: [
-            basicSetup,
-            java(),
+        state: EditorState.create({
+            extensions: [
+                basicSetup,
+                // keymap.of([
+                //     indentWithTab
+                // ]),
+                java(),
 
-        ],
-        doc: original_code
-      }),
-      parent: editor_element
+            ],
+            tabSize: 4,
+            doc: original_code
+        }),
+        parent: editor_element
     });
 
     editor_element.removeAttribute("hidden");
 
-    document.getElementById('submit_code').addEventListener("click", ()=> {
+    document.getElementById('submit_code').addEventListener("click", () => {
         // submitting the code
         let code = editor.state.doc.toString();
         let b64code = btoa(code);
