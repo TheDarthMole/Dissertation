9b0d02a807f270a133cecc5bfd48ca3ec512d817
TheDarthMole <27959011+TheDarthMole@users.noreply.github.com>
2022-05-11 12:40:18 +0100

    feat: Added lesson tests

--- apps/learn/tests.py
+++ apps/learn/tests.py
@@ -1,3 +1,63 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.test import Client
+from apps.learn.models import Lesson, ExploitType, Difficulty
+from apps.containers.models import Container, Image
+import http.client
 
-# Create your tests here.
+
+class LearnTestCase(TestCase):
+    def setUp(self):
+        credentials = {
+            'username': 'test_user',
+            'password': 'wwGx5ZSZS3jNBvByqECx6H2pWSkqB39Z'
+        }
+        self.client = Client()
+        user_model = get_user_model()
+        self.test_exploit = ExploitType(title="example exploit",
+                                        slug="example_exploit")
+        self.test_exploit.save()
+
+        self.lesson = Lesson(title="Sample lesson",
+                             slug="sample_lesson",
+                             exploit_type=self.test_exploit,
+                             overview="Overview here",
+                             difficulty=Difficulty.normal,
+                             content="# H1 Content\n\n## H2 Content\n\nThis is more content")
+        self.lesson.save()
+
+        self.user = user_model.objects.create_user(credentials['username'], 'test@example.com', credentials['password'])
+        self.client.login(**credentials)
+
+    def test_lessons_exist(self):
+        data = self.client.get("/learn")
+        self.assertEqual(data.status_code, http.HTTPStatus.OK)
+        self.assertContains(data, "Learn")
+
+    def test_lesson_exist(self):
+        data = self.client.get(f"/lesson/{self.lesson.slug}")
+        self.assertEqual(data.status_code, http.HTTPStatus.OK)
+
+    def test_lesson_contains_lesson(self):
+        data = self.client.get(f"/lesson/{self.lesson.slug}")
+        self.assertContains(data, "H1 Content")
+        self.assertContains(data, "H2 Content")
+        self.assertContains(data, "This is more content")
+
+    def test_lessons_contains_exploits(self):
+        data = self.client.get("/learn")
+        self.assertContains(data, self.lesson.exploit_type)
+
+    def test_lesson_complete(self):
+        # Check lesson isn't completed already
+        data = self.client.get(f"/lesson/{self.lesson.slug}")
+        self.assertContains(data, "Complete lesson")
+
+        # Complete lesson
+        data1 = self.client.get(f"/complete_lesson/{self.lesson.slug}")
+        self.assertEqual(data1.status_code, http.HTTPStatus.FOUND)
+
+        # Make sure lesson is completed
+        data2 = self.client.get(f"/lesson/{self.lesson.slug}")
+        self.assertContains(data2, "Lesson already completed!")
+
