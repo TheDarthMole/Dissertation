002889ab0beeedb179cb2aed3f9c125214890178
TheDarthMole <27959011+TheDarthMole@users.noreply.github.com>
2022-05-16 17:00:56 +0100

    refactor: Removed redundant lists that aren't used

--- apps/learn/urls.py
+++ apps/learn/urls.py
@@ -4,10 +4,8 @@ from django.urls import path, re_path
 from apps.learn import views
 
 urlpatterns = [
-
-    # The containers page
+    # The learn page
     path('learn', views.learn, name='learn'),
     path('lesson/<slug:slug>', views.LessonDetailedView.as_view(), name='lesson_detail'),
     path('complete_lesson/<slug:slug>', views.complete_lesson, name='complete_lesson')
-
 ]
