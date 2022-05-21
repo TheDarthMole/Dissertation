ea22aaa8c28f4c3e6b920d15eb71c633d41b53bd
TheDarthMole <27959011+TheDarthMole@users.noreply.github.com>
2022-05-21 22:03:16 +0100

    chore: Added a test for when code doesn't compile

--- apps/containers/tests.py
+++ apps/containers/tests.py
@@ -11,6 +11,7 @@ code = open("docker_files/Dockerfiles/Java/LFI/FileReader/src/main/java/FileRead
 # code_base64 = base64.b32encode(code.encode("utf-8"))
 code_base64 = base64.b64encode(bytes(code, 'utf-8')).decode("utf-8")
 code_fixed = "aW1wb3J0IGphdmEuaW8uRmlsZTsKaW1wb3J0IGphdmEuaW8uRmlsZU5vdEZvdW5kRXhjZXB0aW9uOwppbXBvcnQgamF2YS51dGlsLlNjYW5uZXI7CgpwdWJsaWMgY2xhc3MgRmlsZVJlYWRlciB7CgogICAgcHJpdmF0ZSBTdHJpbmcgZmlsZU5hbWU7CgogICAgRmlsZVJlYWRlcihTdHJpbmcgZmlsZU5hbWUpIHsKICAgICAgICB0aGlzLmZpbGVOYW1lID0gZmlsZU5hbWU7CiAgICB9CgogICAgLyoqCiAgICAgKiBBIHNpbXBsZSBmaWxlIHJlYWRlcgogICAgICogQHJldHVybiBUaGUgY29udGVudHMgb2YgdGhlIGZpbGUgcHJvdmlkZWQKICAgICAqIEB0aHJvd3MgRmlsZU5vdEZvdW5kRXhjZXB0aW9uIElmIHRoZSBmaWxlIGlzIG5vdCBmb3VuZCwgb3IgaWYgdGhlIGZpbGUgc2hvdWxkIG5vdCBiZSByZWFkCiAgICAgKi8KICAgIHB1YmxpYyBTdHJpbmcgcmVhZEZpbGUoKSB0aHJvd3MgRmlsZU5vdEZvdW5kRXhjZXB0aW9uIHsKICAgICAgICAvLyBPbmx5IHJlYWQgZnJvbSBmaWxlcyBkaXJlY3RseSB3aXRoaW4gdGhlIGFzc2V0cyBkaXJlY3RvcnkKICAgICAgICBTdHJpbmcgcHJlZml4ID0gImFzc2V0cy8iOwoKICAgICAgICBpZiAodGhpcy5maWxlTmFtZS5jb250YWlucygiLyIpKSB7CiAgICAgICAgICAgIHRocm93IG5ldyBGaWxlTm90Rm91bmRFeGNlcHRpb24oKTsKICAgICAgICB9CgogICAgICAgIEZpbGUgZmlsZSA9IG5ldyBGaWxlKHByZWZpeCArIGZpbGVOYW1lKTsKICAgICAgICBTdHJpbmdCdWlsZGVyIHJldFZhbHVlID0gbmV3IFN0cmluZ0J1aWxkZXIoKTsKICAgICAgICBTY2FubmVyIHNjYW5uZXIgPSBuZXcgU2Nhbm5lcihmaWxlKTsKCiAgICAgICAgd2hpbGUgKHNjYW5uZXIuaGFzTmV4dExpbmUoKSkgewogICAgICAgICAgICAvLyBBZGQgdGhlIHRleHQgdG8gdGhlIHJldHVybiBzdHJpbmcKICAgICAgICAgICAgcmV0VmFsdWUuYXBwZW5kKHNjYW5uZXIubmV4dExpbmUoKSk7CgogICAgICAgICAgICAvLyBBZGQgYSBuZXcgbGluZSBjaGFyYWN0ZXIgaWYgdGhlcmUgaXMgYSBuZXcgbGluZSEKICAgICAgICAgICAgaWYgKHNjYW5uZXIuaGFzTmV4dExpbmUoKSkKICAgICAgICAgICAgICAgIHJldFZhbHVlLmFwcGVuZCgiXG4iKTsKICAgICAgICB9CiAgICAgICAgU3lzdGVtLm91dC5wcmludGxuKHJldFZhbHVlKTsKICAgICAgICByZXR1cm4gcmV0VmFsdWUudG9TdHJpbmcoKTsKICAgIH0KCiAgICBwdWJsaWMgc3RhdGljIHZvaWQgbWFpbihTdHJpbmdbXSBhcmdzKSB0aHJvd3MgRmlsZU5vdEZvdW5kRXhjZXB0aW9uIHsKICAgICAgICBGaWxlUmVhZGVyIGZyID0gbmV3IEZpbGVSZWFkZXIoInNhbXBsZS50eHQiKTsKCiAgICAgICAgU3lzdGVtLm91dC5wcmludGxuKGZyLnJlYWRGaWxlKCkpOwogICAgfQp9Cg=="
+code_no_compile = "Q29weXJpZ2h0IE5pY2sgUnVmZmxlcyAoQyk="
 description = "# Test challenge\n## SubHeading\nCan you find the vulnerability in the file reader??\n\nIf the readFile method smells something " \
               "fishy going on, then it should throw a FileNotFoundException. "
 
@@ -49,10 +50,17 @@ class ContainerTestCase(TestCase):
         self.assertContains(data, "lfi1_fileread:latest")
 
     def test_challenge_start(self):
+        # Test to make sure that no containers exist already
+        containers = Container.objects.filter(owner_id=self.user.id,
+                                              container_image=self.test_challenge)
+        self.assertEqual(len(containers), 0)
+
         data = self.client.post("/challenges/start", {"imageID": str(self.test_challenge.id)})
         containers = Container.objects.filter(owner_id=self.user.id,
                                               container_image=self.test_challenge)
+        # Test to make sure the api inserted the new entry successfully
         self.assertEqual(len(containers), 1)
+        self.assertEqual(data.status_code, http.HTTPStatus.FOUND)
 
     def test_challenge_contains_description(self):
         data = self.client.post("/challenges/start", {"imageID": str(self.test_challenge.id)})
@@ -89,5 +97,10 @@ class ContainerTestCase(TestCase):
     def test_challenge_submit_pass(self):
         self.challenge_test(self.test_challenge, http.HTTPStatus.FOUND, code_fixed,
                             "/challenges", "Well done! You completed File Reader!")
+
+    def test_challenge_fails_compile(self):
+        self.challenge_test(self.test_challenge, http.HTTPStatus.FOUND, code_no_compile,
+                            "/challenge/",
+                            "Your code failed to compile!")
 
 
